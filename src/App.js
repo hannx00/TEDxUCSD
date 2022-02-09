65db951b578a1d75dd168de57a6f7a4ad019ab2d
hannguyennn <han001@ucsd.edu>
2022-02-08 19:05:00 -0800

    update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { render } from '@testing-library/react';
 class App extends React.Component {
   render() {
     return(
-      <HashRouter>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Navbar />
         <Switch>
           <Route exact path="/">
